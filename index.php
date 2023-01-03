d4fe77ba5dc9c21f33ff5658ad883d7e5e99259e
MrQGamer13 <112868405+VenghongTaing@users.noreply.github.com>
2023-01-03 08:45:13 +0000

    modified:   index.php

--- index.php
+++ index.php
@@ -2,40 +2,37 @@
 session_start();
 error_reporting(1);
 include("includes/config.php");
-if(isset($_POST['submit']))
-{
-    $regno=$_POST['regno'];
-    $password=md5($_POST['password']);
-$query=mysqli_query($bd, "SELECT * FROM students WHERE StudentRegno='$regno' and password='$password'");
-if(mysqli_num_rows($query)>0)
-{
-$num=mysqli_fetch_array($query);
-$extra="change-password.php";//
-$_SESSION['login']=$_POST['regno'];
-$_SESSION['id']=$num['studentRegno'];
-$_SESSION['sname']=$num['studentName'];
-$uip=$_SERVER['REMOTE_ADDR'];
-$status=1;
-$log=mysqli_query($bd, "insert into userlog(studentRegno,userip,status) values('".$_SESSION['login']."','$uip','$status')");
-$host=$_SERVER['HTTP_HOST'];
-$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-header("location:http://$host$uri/$extra");
-exit();
-}
-else
-{
-$_SESSION['errmsg']="Invalid Reg no or Password";
-$extra="index.php";
-$host  = $_SERVER['HTTP_HOST'];
-$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-header("location:http://$host$uri/$extra");
-exit();
-}
+if (isset($_POST['submit'])) {
+    $regno = $_POST['regno'];
+    $password = md5($_POST['password']);
+    $query = mysqli_query($bd, "SELECT * FROM students WHERE StudentRegno='$regno' and password='$password'");
+    if (mysqli_num_rows($query) > 0) {
+        $num = mysqli_fetch_array($query);
+        $extra = "change-password.php"; //
+        $_SESSION['login'] = $_POST['regno'];
+        $_SESSION['id'] = $num['studentRegno'];
+        $_SESSION['sname'] = $num['studentName'];
+        $uip = $_SERVER['REMOTE_ADDR'];
+        $status = 1;
+        $log = mysqli_query($bd, "insert into userlog(studentRegno,userip,status) values('" . $_SESSION['login'] . "','$uip','$status')");
+        $host = $_SERVER['HTTP_HOST'];
+        $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        header("location:http://$host$uri/$extra");
+        exit();
+    } else {
+        $_SESSION['errmsg'] = "Invalid Reg no or Password";
+        $extra = "index.php";
+        $host = $_SERVER['HTTP_HOST'];
+        $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+        header("location:http://$host$uri/$extra");
+        exit();
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
@@ -47,8 +44,9 @@ exit();
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
 </head>
+
 <body>
-    <?php include('includes/header.php');?>
+    <?php include('includes/header.php'); ?>
     <div class="content-wrapper">
         <div class="container">
             <div class="row">
@@ -58,19 +56,22 @@ exit();
                 </div>
 
             </div>
-             <span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
+            <span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?>
+                <?php echo htmlentities($_SESSION['errmsg'] = ""); ?>
+            </span>
             <form name="admin" method="post">
-            <div class="row">
-                <div class="col-md-6">
-                     <label>Enter Reg no : </label>
-                        <input type="text" name="regno" class="form-control"  />
-                        <label>Enter Password :  </label>
-                        <input type="password" name="password" class="form-control"  />
+                <div class="row">
+                    <div class="col-md-6">
+                        <label>Enter Reg no : </label>
+                        <input type="text" name="regno" class="form-control" />
+                        <label>Enter Password : </label>
+                        <input type="password" name="password" class="form-control" />
                         <hr />
-                        <button type="submit" name="submit" class="btn btn-info"><span class="glyphicon glyphicon-user"></span> &nbsp;Log Me In </button>&nbsp;
-                </div>
-                </form>
-                <!-- <div class="col-md-6">
+                        <button type="submit" name="submit" class="btn btn-info"><span
+                                class="glyphicon glyphicon-user"></span> &nbsp;Log Me In </button>&nbsp;
+                    </div>
+            </form>
+            <!-- <div class="col-md-6">
                     <div class="alert alert-info">
                         This is a free bootstrap admin template with basic pages you need to craft your project. 
                         Use this template for free to use for personal and commercial use.
@@ -94,14 +95,15 @@ exit();
                     </div>
                 </div> -->
 
-            </div>
         </div>
     </div>
- 
-    <?php include('includes/footer.php');?>
-   
+    </div>
+
+    <?php include('includes/footer.php'); ?>
+
     <script src="assets/js/jquery-1.11.1.js"></script>
 
     <script src="assets/js/bootstrap.js"></script>
 </body>
+
 </html>
