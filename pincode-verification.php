94e22bb4bcd95563072753a781944c1a5f8098b2
Mon Mat <monsokmeanmon@gmail.com>
2023-01-08 22:51:39 +0700

    Pincode is already

--- pincode-verification.php
+++ pincode-verification.php
@@ -1,96 +1,91 @@
-
 <?php
 session_start();
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-    {   
-header('location:index.php');
-}
-else{
-date_default_timezone_set('Asia/Kolkata');// change according timezone
-$currentTime = date( 'd-m-Y h:i:s A', time () );
-if(isset($_POST['submit']))
-{
-$sql=mysqli_query($bd, "SELECT * FROM  students where pincode='".trim($_POST['pincode'])."' && StudentRegno='".$_SESSION['login']."'");
-$num=mysqli_fetch_array($sql);
-if($num>0)
-{
-$_SESSION['pcode']=$_POST['pincode'];
-header("location:enroll.php");
-}
-else
-{
-$_SESSION['msg']="Error :Wrong Pincode. Please Enter a Valid Pincode !!";
-}
-}
-?>
+if (strlen($_SESSION['login']) == 0) {
+    header('location:index.php');
+} else {
+    date_default_timezone_set('Asia/Kolkata');// change according timezone
+    $currentTime = date('d-m-Y h:i:s A', time());
+    if (isset($_POST['submit'])) {
+        $sql = mysqli_query($bd, "SELECT * FROM  students where pincode='" . trim($_POST['pincode']) . "' && StudentRegno='" . $_SESSION['login'] . "'");
+        $num = mysqli_fetch_array($sql);
+        if ($num > 0) {
+            $_SESSION['pcode'] = $_POST['pincode'];
+            header("location:enroll.php");
+        } else {
+            $_SESSION['msg'] = "Error :Wrong Pincode. Please Enter a Valid Pincode !!";
+        }
+    }
+    ?>
 
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Pincode Verification</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <link href="assets/css/style.css" rel="stylesheet" />
-</head>
+    <!DOCTYPE html>
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        <meta name="description" content=""/>
+        <meta name="author" content=""/>
+        <title>Pincode Verification</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="assets/css/font-awesome.css" rel="stylesheet"/>
+        <link href="assets/css/style.css" rel="stylesheet"/>
+    </head>
 
-<body>
-<?php include('includes/header.php');?>
-    
-<?php if($_SESSION['login']!="")
-{
- include('includes/menubar.php');
-}
- ?>
-   
+    <body>
+    <?php include('includes/header.php'); ?>
+
+    <?php if ($_SESSION['login'] != "") {
+        include('includes/menubar.php');
+    }
+    ?>
+
     <div class="content-wrapper">
         <div class="container">
-              <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="page-head-line">Student Pincode Verification</h1>
-                    </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="page-head-line">Student Pincode Verification</h1>
                 </div>
-                <div class="row" >
-                  <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        <div class="panel panel-default">
-                        <div class="panel-heading">
-                          Pincode Verification
+            </div>
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading badge bg-danger text-wrap text-uppercase fs-5 font-monospace d-flex justify-content-center">
+
+                            Pincode Verification
                         </div>
-<font color="red" align="center"><?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?></font>
+                        <font color="red"
+                              align="center"><?php echo htmlentities($_SESSION['msg']); ?><?php echo htmlentities($_SESSION['msg'] = ""); ?></font>
+
+                        <div class="card">
+                            <div class="card-body"  style="box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;">
+                                <form name="pincodeverify" method="post">
+                                    <div class="form-group">
+                                        <label for="pincode" class="mt-3 font-monospace fw-bold card-title">Enter Pincode</label>
+                                        <input type="password" class="form-control" id="pincode" name="pincode"
+                                               placeholder="Pincode" required/>
+                                    </div>
+
+                                    <button type="submit" name="submit" class="btn btn-danger mt-3" style="border-radius: 15px">Verify</button>
+                                    <hr/>
 
 
-                        <div class="panel-body">
-                       <form name="pincodeverify" method="post">
-   <div class="form-group">
-    <label for="pincode">Enter Pincode</label>
-    <input type="password" class="form-control" id="pincode" name="pincode" placeholder="Pincode" required />
-  </div>
- 
-  <button type="submit" name="submit" class="btn btn-default">Verify</button>
-                           <hr />
-   
-
-
-
-</form>
+                                </form>
                             </div>
-                            </div>
+                        </div>
+
                     </div>
-                  
                 </div>
+
+            </div>
         </div>
     </div>
-    
-  <?php include('includes/footer.php');?>
-   
+
+    <?php include('includes/footer.php'); ?>
+
     <script src="assets/js/jquery-1.11.1.js"></script>
 
     <script src="assets/js/bootstrap.js"></script>
-</body>
-</html>
+    </body>
+    </html>
 <?php } ?>
