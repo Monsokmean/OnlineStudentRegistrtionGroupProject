748dcdad1c5ee9801b2851b508db69ecdbbf5843
MrQGamer13 <112868405+VenghongTaing@users.noreply.github.com>
2023-01-09 07:26:45 +0000

    modified:   enroll-history.php

--- enroll-history.php
+++ enroll-history.php
@@ -36,14 +36,11 @@ if (strlen($_SESSION['login']) == 0) {
                 </div>
             </div>
             <div class="row">
-
                 <div class="col-md-12">
-
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Enroll History
                         </div>
-
                         <div class="panel-body">
                             <div class="table-responsive table-bordered">
                                 <table class="table">
@@ -59,36 +56,46 @@ if (strlen($_SESSION['login']) == 0) {
                                             <th>Action</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        <?php
-                                        $sql = mysqli_query($bd, "select courseenrolls.course as cid, course.courseName as courname,session.session as session,department.department as dept,level.level as level,courseenrolls.enrollDate as edate ,semester.semester as sem from courseenrolls join course on course.id=courseenrolls.course join session on session.id=courseenrolls.session join department on department.id=courseenrolls.department join level on level.id=courseenrolls.level  join semester on semester.id=courseenrolls.semester  where courseenrolls.studentRegno='" . $_SESSION['login'] . "'");
-                                        $cnt = 1;
-                                        while ($row = mysqli_fetch_array($sql)) {
-                                            ?>
-                                            <tr>
-                                                <td><?php echo $cnt; ?></td>
-                                                <td>
-                                                    <?php echo htmlentities($row['courname']); ?>
-                                                </td>
-                                                <td><?php echo htmlentities($row['session']); ?></td>
-                                                <td>
-                                                    <?php echo htmlentities($row['dept']); ?>
-                                                </td>
-                                                <td><?php echo htmlentities($row['level']); ?></td>
-                                                <td>
-                                                    <?php echo htmlentities($row['sem']); ?>
-                                                </td>
-                                                <td><?php echo htmlentities($row['edate']); ?></td>
-                                                <td>
-                                                    <a href="print.php?id=<?php echo $row['cid'] ?>" target="_blank">
-                                                        <button class="btn btn-primary"><i class="fa fa-print "></i>
-                                                            Print</button> </a>
-                                                </td>
-                                            </tr>
-                                            <?php
-                                            $cnt++;
-                                        } ?>
-                                    </tbody>
+        <tbody>
+            <?php
+            $sql = mysqli_query($bd, "SELECT courseenrolls.course AS cid, 
+                                course.courseName AS courname, session.session AS session,
+                                department.department AS dept, level.level AS level,
+                                courseenrolls.enrollDate AS edate, semester.semester AS sem 
+                                FROM courseenrolls 
+                                JOIN course ON course.id = courseenrolls.course 
+                                JOIN session ON session.id = courseenrolls.session 
+                                JOIN department ON department.id = courseenrolls.department 
+                                JOIN level ON level.id = courseenrolls.level 
+                                JOIN semester on semester.id = courseenrolls.semester 
+                                WHERE courseenrolls.studentRegno = '" . $_SESSION['login'] . "'");
+            $cnt = 1;
+            while ($row = mysqli_fetch_array($sql)) {
+                ?>
+                <tr>
+                    <td><?php echo $cnt; ?></td>
+                    <td>
+                        <?php echo htmlentities($row['courname']); ?>
+                    </td>
+                    <td><?php echo htmlentities($row['session']); ?></td>
+                    <td>
+                        <?php echo htmlentities($row['dept']); ?>
+                    </td>
+                    <td><?php echo htmlentities($row['level']); ?></td>
+                    <td>
+                        <?php echo htmlentities($row['sem']); ?>
+                    </td>
+                    <td><?php echo htmlentities($row['edate']); ?></td>
+                    <td>
+                        <a href="print.php?id=<?php echo $row['cid'] ?>" target="_blank">
+                            <button class="btn btn-primary"><i class="fa fa-print "></i>
+                                Print</button> </a>
+                    </td>
+                </tr>
+                <?php
+                $cnt++;
+            } ?>
+        </tbody>
                                 </table>
                             </div>
                         </div>
