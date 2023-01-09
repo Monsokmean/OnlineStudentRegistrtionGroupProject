0fe1286bad7295894fed211832c54c05a6ea0ce0
MrQGamer13 <112868405+VenghongTaing@users.noreply.github.com>
2023-01-09 07:58:10 +0000

    modified:   logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,17 @@
 <?php
-session_start();
-include("includes/config.php");
-$_SESSION['login']="";
-date_default_timezone_set('Asia/Phnom_Penh');
-$ldate=date( 'd-m-Y h:i:s A', time () );
-mysqli_query($bd, "UPDATE userlog  SET logout = '$ldate' WHERE studentRegno = '".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
-session_unset();
-$_SESSION['errmsg']="You have successfully logout";
+    session_start();
+    include("includes/config.php");
+
+    $_SESSION['login'] = "";
+    date_default_timezone_set('Asia/Phnom_Penh');
+
+    $ldate = date('d-m-Y h:i:s A', time());
+    mysqli_query($bd, "UPDATE userlog 
+                        SET logout = '$ldate' 
+                        WHERE studentRegno = '" . $_SESSION['login'] . "' ORDER BY id DESC LIMIT 1");
+    session_unset();
+    $_SESSION['errmsg'] = "You have successfully logout";
 ?>
 <script language="javascript">
-document.location="index.php";
+    document.location = "index.php";
 </script>
